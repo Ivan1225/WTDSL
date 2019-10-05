9dfcd2c9610d2ea9849095788b0a5500a71829f0
Frank Yan <frankyan1129@gmail.com>
2019-10-05 16:29:36 -0700

    add tonkenizer and tokens

---
+++ src/libs/Tokens.ts
@@ -0,0 +1,39 @@
+export default class Tokens {
+    public static WITHIN = "Within";
+
+    public static ENDWITHIN = "EndWithin";
+
+    public static VISIT = "Visit";
+
+    public static SELECT = "Select";
+
+    public static EXPECT = "Expect";
+
+    public static VALUE= "Value";
+
+    public static OF = "of";
+
+    public static IS = "is";
+
+    public static CLICK = "CLick";
+
+    public static SHOULD = "should";
+
+    public static BE = "be";
+
+    public static WAIT = "Wait";
+
+    public static FILL = "Fill";
+
+    public static NOT = "not";
+
+    public static Contain  = "contain";
+    
+    public static ATTRIBUTE = "\[.*\]";
+
+    public static FROM = "from";
+
+    public static STRING = "\".*\"";
+
+    public static NUMMBER = "[0-9]+";
+}
