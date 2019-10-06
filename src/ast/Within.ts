561126c116dbf7fbbb8eeb7303839cb5cb261b21
Frank Yan <frankyan1129@gmail.com>
2019-10-06 13:31:56 -0700

    handle multi within case

--- src/ast/Within.ts
+++ src/ast/Within.ts
@@ -21,13 +21,16 @@ export default class Within extends Node {
 
         this.selector = Utils.trimCurlyBraces(tokenizer.pop());
 
-        while(tokenizer.hasNext() && tokenizer.top() !== Tokens.ENDWITHIN) {
+        while(tokenizer.hasNext() && tokenizer.top() !== Tokens.ENDWITHIN && tokenizer.top() !== null) {
             let s: Node = Statement.getSubStatement(tokenizer);
             s.parse(tokenizer);
             this.statements.push(s);
         }
 
-        tokenizer.pop();
+        currentLine = tokenizer.getLine();
+        if(tokenizer.pop() !== Tokens.ENDWITHIN) {
+            throw new ParserError(`Missing EndWithin keyword at line ${currentLine}.`);
+        }
     }    
     
     public evaluate() {
