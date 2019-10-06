a9d89e8209a87d0e1edeaf51e05da32853172dd6
Ivan Zhang <z243155476@gmail.com>
2019-10-05 19:19:39 -0700

    Fix validate url

--- src/ast/Visit.ts
+++ src/ast/Visit.ts
@@ -1,19 +1,29 @@
 import {ParserError} from '../errors/ParserError';
 import Tokenizer from "../libs/Tokenizer";
-import Statement from "./Statement";
+import {Node} from "./Node";
 
-export default class Visit extends Statement{
-    url: URL;
+export default class Visit extends Node{
+    url: string;
 
     public parse(tokenizer: Tokenizer) {
-      tokenizer.top();
-      try {
-        this.url = new URL(tokenizer.top().replace(/"/g,""));
-      } catch {
+      tokenizer.pop();
+      this.url = tokenizer.pop().replace(/"/g,"");
+
+      if (!this.validURL(this.url)) {
         throw new ParserError("Invalid URL");
       }
     }
 
     public evaluate() {
+    }
+
+    private validURL(str) {
+      const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+      return !!pattern.test(str);
     }
 }
