6b7a8988ba0601345f19b6f24b83df7ecab60bc0
David Chen <zeyuchen97@hotmail.com>
2019-10-12 16:48:33 -0700

    Write test result

--- src/ast/Node.ts
+++ src/ast/Node.ts
@@ -1,4 +1,6 @@
 import Tokenizer from "../libs/Tokenizer";
+import * as fs from 'fs';
+import * as path from 'path';
 
 export abstract class Node {
 
@@ -6,26 +8,40 @@ export abstract class Node {
 	public static selector;
     public static page;
     public static withinPrefixes: string[] = [];
-    protected static testResult: {[key: string]: number} = {
+    protected static testStats: {[key: string]: number} = {
         total: 0,
         pass: 0,
         fail: 0,
     }
+    protected static failedTests: number[] = [];
     protected static testPass() {
-        Node.testResult.total += 1;
-        Node.testResult.pass += 1;
+        Node.testStats.total += 1;
+        Node.testStats.pass += 1;
 		console.log('passed test');
     }
     protected static testFail() {
-        Node.testResult.total += 1;
-        Node.testResult.fail += 1;
+        Node.testStats.total += 1;
+        Node.testStats.fail += 1;
+        Node.failedTests.push(Node.testStats.total);
 		console.log('failed test');
     }
     protected static printResult() {
-        let pass : number = Node.testResult.pass;
-        let fail : number = Node.testResult.fail;
-        let total : number = Node.testResult.total
-        console.log(`Ran ${total} test(s) in total; ${pass} test(s) passed; ${fail} test(s) failed`);
+        let pass : number = Node.testStats.pass;
+        let fail : number = Node.testStats.fail;
+        let total : number = Node.testStats.total
+        let failedArr = Node.failedTests;
+        let testStatsSummary = `Ran ${total} test(s) in total; ${pass} test(s) passed; ${fail} test(s) failed`;
+        let failedTestsSummary = `Failed tests are: test ${failedArr.reduce((acc, curr) => `${curr}${acc ? `, ${acc}` : acc}`, '')}`
+        let output = [testStatsSummary];
+        if (Node.failedTests.length > 0) {
+            output.push(failedTestsSummary);
+        }
+        let date = new Date();
+        fs.writeFileSync(path.join(__dirname, "../../resources/output/testResult.txt"), `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}\n`)
+        output.map(data => {
+            console.log(data);
+            fs.appendFileSync(path.join(__dirname, "../../resources/output/testResult.txt"), `${data}\n`);
+        });
     }
     protected static setPage(p) {
         Node.page = p;
